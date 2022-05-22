a419749f02642f39fabdc52c20ee2eff61fb7479
Olga Rybnikova <olga.rybnikova777817@icloud.com>
2022-05-22 17:42:18 +0300

    Employee_calculatePay

--- src/Employee.java
+++ src/Employee.java
@@ -1,36 +1,37 @@
-public class Employee {
-  // TODO fix class declaration and declare variables here
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+ public abstract class Employee implements Payable {
+  public String employeeId;
+  public String name;
 
   public Employee(String employeeId, String name) {
-    // TODO fill in code here
+    this.employeeId = employeeId;
+    this.name = name;
   }
 
   public String getEmployeeId() {
-    // TODO fill in code here and replace the return statement
-    return "";
+    return this.employeeId;
   }
 
   public void setEmployeeId(String employeeId) {
-    // TODO fill in code here
+    this.employeeId = employeeId;
   }
 
   public String getName() {
-    // TODO fill in code here and replace the return statement
-    return "";
+    return this.name;
   }
 
   public void setName(String name) {
-    // TODO fill in code here
+    this.name = name;
   }
 
-  public double getAverageMonthlySalary() {
-    // TODO fill in code here and replace the return statement
-    return 0;
-  }
+  public abstract double getAverageMonthlySalary();
 
   @Override
   public String toString() {
-    // TODO fill in code here and replace the return statement, be sure to format double value
-    return "";
+    return name + " " + employeeId + " " + String.format("%.2f", this.getAverageMonthlySalary());
   }
+
+  public void calculatePay() { }
 }
